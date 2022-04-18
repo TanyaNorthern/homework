242ccda842591e67e667544f7b2e926cad7537ba
irlandskaya.tatyana@yandex.ru <irlandskaya.tatyana@yandex.ru>
2022-04-18 12:19:27 +0300

    дз 1.4

--- main.js
+++ main.js
@@ -1,60 +1,128 @@
 console.clear();
 
 // Задание 1
-console.log('Задание 1');
-for (let i = 0; i < 11; i++) {
-    if (i === 0) {
-        console.log('0 - это ноль');
-    } else {
-        if (i % 2 === 0) {
-            console.log(`${i} – четное число`);
-        } else {
-            console.log(`${i} – нечетное число`);
-        }
+const numbers1 = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+};
+
+for (const key in numbers1) {
+    if (numbers1[key] >= 3) {
+        console.log(numbers1[key]);
     }
 }
-console.log('');
+
 
 // Задание 2
-console.log('Задание 2');
-const array2 = [1, 2, 3, 4, 5, 6, 7];
-array2.splice(3, 2);
-console.log(array2);
-console.log('');
+const post = {
+    author: "John", // вывести этот текст
+    postId: 23,
+    comments: [{
+        userId: 10,
+        userName: "Alex",
+        text: "lorem ipsum",
+        rating: {
+            likes: 10,
+            dislikes: 2, // вывести это число
+        },
+    },
+    {
+        userId: 5, // вывести это число
+        userName: "Jane",
+        text: "lorem ipsum 2", // вывести этот текст
+        rating: {
+            likes: 3,
+            dislikes: 1,
+        },
+    },
+    ],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
 
 // Задание 3
-console.log('Задание 3');
-const array3 = [];
-for (let i = 0; i < 5; i++) {
-    array3.push(Math.floor(Math.random() * 11));
-}
-console.log(array3);
-const array3New = array3.map(item => item ** 2);
-console.log(array3New);
-console.log('');
+const products3 = [{
+    id: 3,
+    price: 200,
+},
+{
+    id: 4,
+    price: 900,
+},
+{
+    id: 1,
+    price: 1000,
+},
+];
+
+products3.forEach(item => item.price *= 0.85);
+console.log(products3);
 
 // Задание 4
-console.log('Задание 4');
-const array4 = [];
-for (let i = 0; i < 5; i++) {
-    array4.push(Math.floor(Math.random() * 11));
-}
-console.log(array4);
-const array4New = array4
-    .filter(item => item !== 3)
-    .map(item => Math.floor((item * 1.1) * 10) / 10); // a + a * 0.1 = a * (1 + 0.1)
-console.log(array4New);
-console.log();
+const products4 = [{
+    id: 3,
+    price: 127,
+    photos: [
+        "1.jpg",
+        "2.jpg",
+    ],
+},
+{
+    id: 5,
+    price: 499,
+    photos: [],
+},
+{
+    id: 10,
+    price: 26,
+    photos: [
+        "3.jpg",
+    ],
+},
+{
+    id: 8,
+    price: 78,
+},
+];
+
+const products4New = products4.filter(item => 'photos' in item && item['photos'].length > 0);
+console.log(products4New);
 
 // Задание 5
-console.log('Задание 5');
-for (let i = 0; i < 10; console.log(i), i++) { /* здесь пусто */ }
-console.log('');
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+
+const daysOfWeek = {};
+for (let i = 0; i < en.length; i++) {
+    daysOfWeek[en[i]] = ru[i];
+}
+console.log(daysOfWeek);
 
 // Необязательное задание
-console.log('Необязательное задание');
-let str = '*';
-for (let i = 0; i < 20; i++) {
-    console.log(str);
-    str += '*';
+const numbersE = {
+    key1: {
+        keyin1: 1,
+        keyin2: 2,
+        keyin3: 3,
+    },
+    key2: {
+        keyin1: 4,
+        keyin2: 5,
+        keyin3: 6,
+    },
+};
+
+// 1.1
+let sumOf = 0;
+for (key in numbersE) {
+    sumOf += Object.values(numbersE[key]).reduce((prev, curr) => prev + curr, 0);
 }
+console.log(sumOf);
